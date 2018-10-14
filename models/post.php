61d253259cf27095485de6dfe274eacaa3dc5d5c
shawki vai <shawkiahmed40@gmail.com>
2018-10-14 23:44:14 +0600

    creating Databse

---
+++ models/post.php
@@ -0,0 +1,19 @@
+<?php
+class Post{
+    //DB
+    private $conn;
+    private $table='posts';
+
+    //properties
+    public $id;
+    public $category_id;
+    public $category_name;
+    public $title;
+    public $body;
+    public $author;
+    public $created_at;
+
+    //Constructor with db
+    
+     
+}
